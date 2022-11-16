8ef8fff2b6ed44a54c7cd48328a6b244a0517f11
Elishitas <arayamariaelisa@gmail.com>
2022-11-16 15:13:52 -0300

    Se sube get /hello

--- rest/rest/src/main/java/com/academia/app/rest/Controller/Controller.java
+++ rest/rest/src/main/java/com/academia/app/rest/Controller/Controller.java
@@ -1,14 +1,16 @@
 package com.academia.app.rest.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
-    @GetMapping(value = "/")
+    @GetMapping("/hello")
     public String hello() {
         return "Hola, mi nombre es Elis" ;
     }
+
 }
 
