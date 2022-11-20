6d096e18ebcd257a69c9df9e27cf25014576f819
Elishitas <arayamariaelisa@gmail.com>
2022-11-19 23:59:44 -0300

    agrego cambios en pom.xml + servicios add

---
+++ rest/rest/src/main/java/com/academia/app/rest/services/StudentService.java
@@ -0,0 +1,37 @@
+package com.academia.app.rest.services;
+
+import com.academia.app.rest.model.domain.StudentDTO;
+import com.academia.app.rest.model.mappers.StudentMapper;
+import com.academia.app.rest.model.repositories.StudentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class StudentService {
+    private final StudentRepository studentRepository;
+    private final StudentMapper studentMapper;
+
+    public StudentService(StudentRepository studentRepository, StudentMapper studentMapper) {
+        this.studentRepository = studentRepository;
+        this.studentMapper = studentMapper;
+    }
+
+    public List<StudentDTO> findAll(){
+        return studentRepository
+                .findAll()
+                .stream()
+                .map(studentEntity -> studentMapper.studentEntityToStudentDTO(studentEntity))
+                .collect(Collectors.toList());
+    }
+    public StudentDTO add(StudentDTO studentDTO){
+        return Optional
+                .ofNullable(studentDTO)
+                .map(dto->studentMapper.studentDTOToStudentEntity(dto))
+                .map(entity->studentRepository.save(entity))
+                .map(entity->studentMapper.studentEntityToStudentDTO(entity))
+                .orElse(new StudentDTO());
+    }
+}
