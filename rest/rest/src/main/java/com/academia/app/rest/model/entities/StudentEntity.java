62e460690bf1852b4eac3c071a14d42a3ede2cc4
Elishitas <arayamariaelisa@gmail.com>
2022-11-19 22:43:55 -0300

    terminando dto

--- rest/rest/src/main/java/com/academia/app/rest/model/entities/StudentEntity.java
+++ rest/rest/src/main/java/com/academia/app/rest/model/entities/StudentEntity.java
@@ -5,11 +5,25 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class StudentEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+
+    @Column(name = "id", columnDefinition = "INT UNSIGNED")
+    private Integer id;
+
+    @Column(name = "name", columnDefinition = "VARCHAR(50)")
+    private String name;
+
+    @Column(name = "surname", columnDefinition = "VARCHAR(50)")
+    private String surname;
+
+    @Column(name = "phone", columnDefinition = "VARCHAR(50)")
+    private String phone;
 }
