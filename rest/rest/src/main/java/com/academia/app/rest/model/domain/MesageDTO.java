54a2d82f935825327bf3c7ec419941b8ee21ffb2
Elishitas <arayamariaelisa@gmail.com>
2022-11-23 21:52:19 -0300

    Cambio el nombre del DTO a Message DTO

--- rest/rest/src/main/java/com/academia/app/rest/model/domain/MesageDTO.java
+++ rest/rest/src/main/java/com/academia/app/rest/model/domain/MesageDTO.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ContactAddedDTO {
+public class MesageDTO {
     private Integer id;
     private String mensaje;
 }
