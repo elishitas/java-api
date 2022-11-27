6df0a3cc40f0387b00fa24ad74988dade90ccfbd
Elishitas <arayamariaelisa@gmail.com>
2022-11-27 13:36:52 -0300

    Saludo frances en el controller

--- rest/rest/src/main/java/com/academia/app/rest/Controller/HelloWordController.java
+++ rest/rest/src/main/java/com/academia/app/rest/Controller/HelloWordController.java
@@ -1,5 +1,7 @@
 package com.academia.app.rest.Controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +10,11 @@ public class HelloWordController {
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello dev";
+        return "Hello World!";
     }
 
+    @GetMapping("/salut")
+    public ResponseEntity<String> salut(){
+        return new ResponseEntity<>("Salut Monde!", HttpStatus.OK);
+    }
 }
