7bac47a8cdcb51de7b5106d161727441ba86a90a
Elishitas <arayamariaelisa@gmail.com>
2022-12-01 15:44:18 -0300

    Se prepara el ambiente para el error del excep.DTO

--- rest/rest/src/main/java/com/academia/app/rest/exceptions/dtos/ErrorMessageDTO.java
+++ rest/rest/src/main/java/com/academia/app/rest/exceptions/dtos/ErrorMessageDTO.java
@@ -1,4 +1,13 @@
 package com.academia.app.rest.exceptions.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ErrorMessageDTO {
+    private String message;
+
 }
