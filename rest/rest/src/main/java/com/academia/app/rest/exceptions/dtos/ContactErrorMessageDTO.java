685c2d2c3c741ccbe1da4de2d1577656be4131ae
Elishitas <arayamariaelisa@gmail.com>
2022-12-01 19:47:13 -0300

    Se prepara el ambiente para el error en el dto, contact

--- rest/rest/src/main/java/com/academia/app/rest/exceptions/dtos/ContactErrorMessageDTO.java
+++ rest/rest/src/main/java/com/academia/app/rest/exceptions/dtos/ContactErrorMessageDTO.java
@@ -1,4 +1,14 @@
 package com.academia.app.rest.exceptions.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ContactErrorMessageDTO {
+    private String message;
+    private String path;
+    private String code;
 }
